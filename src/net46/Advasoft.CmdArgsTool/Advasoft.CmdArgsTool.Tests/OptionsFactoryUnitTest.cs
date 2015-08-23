a62b19f3fb1197ca9c898ab44d6405993416a9f9
Denis Kozlov <deniskozlov@outlook.com>
2015-08-23 16:52:26 +0400

    Added CreateOptions test method

---
+++ src/net46/Advasoft.CmdArgsTool/Advasoft.CmdArgsTool.Tests/OptionsFactoryUnitTest.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advasoft.CmdArgsTool.Fakes;
+using System.Diagnostics;
+
+namespace Advasoft.CmdArgsTool.Tests
+{
+    [TestClass]
+    public class OptionsFactoryUnitTest
+    {
+        [TestMethod]
+        public void CreateOptionsTestMethod()
+        {
+            IOptionsPolicy creationPolicy = new StubIOptionsPolicy()
+            {
+                GetSeparateSymbol = () => { return "--"; }
+            };
+            ILogger logger = new StubILogger()
+            {
+                LogErrorException = (e) => { Trace.TraceInformation(e.Message); }
+            };
+            OptionsFactoryBase optionsFactory = new StubOptionsFactoryBase(creationPolicy, logger)
+            {
+                CreateOptionsByPolicyStringArrayIOptionsPolicy = (a,e) =>
+                {
+                    OptionsBase options = new StubOptionsBase();
+                    return options;
+                }
+            };
+
+            OptionsBase createdOptions = optionsFactory.CreateOptions(new string[] { "" });
+
+            Assert.IsNotNull(createdOptions);
+        }
+    }
+}
